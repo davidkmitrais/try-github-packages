5b3b50c9a066945362b049001cd5cfaca11212e3
davidkmitrais <david.kristiawan@mitrais.com>
2023-09-29 09:57:30 +0800

    add new class

---
+++ DK.MyClassLibrary/DK.MyClassLibrary/DKClassInt1.cs
@@ -0,0 +1,10 @@
+﻿namespace DK.MyClassLibrary
+{
+    public class DKClassInt1
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+    }
+}
